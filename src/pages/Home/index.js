7c2bac35c002027cb3fd8dd5f0c833b63a46fff6
EchoRich <2236393580@qq.com>
2021-08-29 09:18:40 +0800

    add some change test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,6 @@
-/*
- * @Author: your name
- * @Date: 2021-07-01 16:10:31
- * @LastEditTime: 2021-08-18 13:02:40
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \mywebsite\src\pages\Home\index.js
- */
-import React from  'react'
- export default function Home () {
-   var str = "Runoob";
-    return (
-      <div>
-        This is Home
-        <div style={{width: "1000px", height: "800px",background: "red"}}>
- {/* {str.repeat(30000)} */}
-        </div>
-       
-      </div>
-    )
+import React from "react"
+ export default function Home() {
+    return (<div>
+      this i home 
+    </div>)
  }
