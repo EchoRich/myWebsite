b9bcbcd97c733bb5e2bb4c2005ecf12ed2dc1ed2
EchoRich <2236393580@qq.com>
2021-09-06 20:29:16 +0800

    伪元素和伪类

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+var moment = require('moment');
  export default function Home() {
+   let str =" 2018.08.16 10:00"
+    console.log(3333, moment(str).isValid())
     return (<div>
       this i home 
     </div>)
